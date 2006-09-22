b5f2e450ed7e0bfe6009bfeb9d31e051da432424
Tim Mayberry <mojofunk@gmail.com>
2006-09-22 13:31:39 +1000

    Rename two files in libmojo

---
+++ src/libmojo/file_extensions.cpp
@@ -0,0 +1,11 @@
+
+#include <libmojo/file_types.h>
+
+namespace mojo {
+
+const char* const state_file_suffix = ".moj";
+
+const char* const peak_file_suffix = ".pks";
+
+} // namespace mojo
+
---
+++ src/libmojo/file_extensions.hpp
@@ -0,0 +1,15 @@
+
+#ifndef MOJO_FILE_TYPES_INCLUDED
+#define MOJO_FILE_TYPES_INCLUDED
+
+namespace mojo {
+
+extern const char* const state_file_suffix;
+
+extern const char* const peak_file_suffix;
+
+} // namespace mojo
+
+#endif // MOJO_FILE_TYPES_INCLUDED
+
+
