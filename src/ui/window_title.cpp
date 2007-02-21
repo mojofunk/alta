56e18d9a9e6aead58d6895cb9dd1c93f8511a6e5
Tim Mayberry <timbyr@localhost.localdomain>
2007-02-21 15:24:52 +1000

    Added WindowTitle class for maintaining consistancy between window titles.

---
+++ src/ui/window_title.cpp
@@ -0,0 +1,24 @@
+#include "window_title.hpp"
+
+namespace {
+	
+// I don't know if this should be translated.
+const char* const title_separator = " - ";
+
+} // anonymous namespace
+
+namespace gmojo {
+
+WindowTitle::WindowTitle(const string& title)
+	: m_title(title)
+{
+
+}
+
+void
+WindowTitle::operator+= (const string& element)
+{
+	m_title = m_title + title_separator + element;
+}
+
+} // namespace gmojo
