8d02dcc43355b0133caba18ab370509366ab6291
Tim Mayberry <mojofunk@gmail.com>
2007-06-05 14:15:16 +1000

    Remove unused paths* files from mojo.
    Something with similar functionality will be needed at some
    point, but until then...out of sight out of mind.

--- src/mojo/mojo.hpp
+++ src/mojo/mojo.hpp
@@ -3,9 +3,6 @@
 
 #include <mojo/audio_track.hpp>
 #include <mojo/directory_names.hpp>
-#include <mojo/paths.hpp>
-#include <mojo/paths_system.hpp>
-#include <mojo/paths_user.hpp>
 #include <mojo/project_directory.hpp>
 #include <mojo/project_format.hpp>
 #include <mojo/project.hpp>
