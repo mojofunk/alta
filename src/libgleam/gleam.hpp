0c118f6981fcecface86f8093b068a872342f459
Tim Mayberry <timbyr@localhost.localdomain>
2007-08-26 13:04:35 +1000

    Remove libgleam/periodic_signals*
    These might be needed at some point in the future, they depend
    on glibmm and sigc++

--- src/libgleam/gleam.hpp
+++ src/libgleam/gleam.hpp
@@ -6,8 +6,6 @@
 #include <libgleam/atomic_ptr.hpp>
 #include <libgleam/dispatcher.hpp>
 #include <libgleam/manual_dispatcher.hpp>
-#include <libgleam/periodic_signal.hpp>
-#include <libgleam/periodic_signals.hpp>
 #include <libgleam/posix_signals.hpp>
 #include <libgleam/semaphore.hpp>
 #include <libgleam/terminate_process.hpp>
