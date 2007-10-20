cf9bc6eaa8a0ab16cc14e614828d5e005d3f699d
Tim Mayberry <mojofunk@gmail.com>
2007-10-20 19:58:38 +1100

    Add scons files for building cformat tests
    Update the run_tests.sh script to be more generic
    
    Update the src/SConscript file to be slightly more compact(non-verbose?...looking for the right word)

--- run_tests.sh
+++ run_tests.sh
@@ -4,20 +4,13 @@
 
 . gmojo_env.sh
 
-RTG_TEST_DIR=$BUILD_DIR/rtg/tests
+for dir in "rtg" "ark" "libgleam";
+do
+	echo "Running test cases for $dir"
 
-echo "Running test cases for rtg"
-
-$RTG_TEST_DIR/test_graph "$@"
-
-ARK_TEST_DIR=$BUILD_DIR/ark/tests
-
-echo "Running test cases for ark"
-
-$ARK_TEST_DIR/test_dummy_archive "$@"
-
-GLEAM_TEST_DIR=$BUILD_DIR/libgleam/tests
-
-echo "Running test cases for gleam"
-
-$GLEAM_TEST_DIR/test_filesystem_paths "$@"
+	TEST_DIR="$BUILD_DIR/$dir/tests/"
+	for file in `find $TEST_DIR -type f -perm /u+x`;
+	do
+		$file "$@";
+	done;
+done
---
+++ src/cformat/tests/test_cformat.cpp
@@ -0,0 +1,19 @@
+#define BOOST_TEST_MODULE cformat
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
+//#include <boost/test/test_tools.hpp>
+
+#include <cformat/cformat.hpp>
+
+using namespace boost::unit_test;
+using namespace std;
+
+BOOST_AUTO_TEST_CASE( cformat_test )
+{
+	int int_val;
+	float float_val;
+
+	string int_string;
+	string float_string;
+}
