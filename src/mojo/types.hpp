0ba72750cf431db9999f58403cc566fd5456c588
Tim Mayberry <mojofunk@gmail.com>
2007-11-23 22:51:43 +1000

    Add mojo/types.hpp file

---
+++ src/mojo/types.hpp
@@ -0,0 +1,15 @@
+
+#ifndef MOJO_TYPES_INCLUDED
+#define MOJO_TYPES_INCLUDED
+
+#include <inttypes.h>
+
+namespace mojo {
+
+typedef int64_t     count_t;
+typedef uint8_t     channel_count_t;
+typedef uint32_t    samplerate_t;
+
+} // namespace mojo
+
+#endif
