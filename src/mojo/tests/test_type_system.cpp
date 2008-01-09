b76fb68df9b113fce0ab82837b0bb89a7feb35aa
Tim Mayberry <mojofunk@gmail.com>
2008-01-09 11:07:25 +1000

    Update mojo type system test to use mojo::Application to register types

--- src/mojo/tests/test_type_system.cpp
+++ src/mojo/tests/test_type_system.cpp
@@ -5,7 +5,10 @@
 #include <boost/test/unit_test_log.hpp>
 //#include <boost/test/test_tools.hpp>
 
+#include <mojo/application.hpp>
+
 #include <mojo/type_system.hpp>
+#include <mojo/type_names.hpp>
 
 using namespace boost::unit_test;
 using namespace std;
@@ -13,13 +16,12 @@ using namespace mojo;
 
 BOOST_AUTO_TEST_CASE( mojo_test )
 {
-	mojo::type_system_init();
+	int argc = framework::master_test_suite().argc;
+	char** argv = framework::master_test_suite().argv;
 
-	mojo::register_type_name (typeid(int), "int");
-	mojo::register_type_name (typeid(float), "float");
-	mojo::register_type_name (typeid(std::string), "string");
+	ApplicationSPtr app = Application::create (argc, argv);
 
-	BOOST_CHECK(mojo::get_type_name (typeid(int)) == "int");
-	BOOST_CHECK(mojo::get_type_name (typeid(float)) == "float");
-	BOOST_CHECK(mojo::get_type_name (typeid(std::string)) == "string");
+	BOOST_CHECK(mojo::get_type_name (typeid(int)) == int_type_name);
+	BOOST_CHECK(mojo::get_type_name (typeid(float)) == float_type_name);
+	BOOST_CHECK(mojo::get_type_name (typeid(std::string)) == string_type_name);
 }
