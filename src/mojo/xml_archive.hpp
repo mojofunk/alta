51d7bb9bfcfb6d62205b49aa229fd39915b1e45c
Tim Mayberry <mojofunk@gmail.com>
2008-01-11 19:59:26 +1000

    Add comments to XMLArchive class and add tests/project.xml file which is roughly what I think the XML should look like

--- src/mojo/xml_archive.hpp
+++ src/mojo/xml_archive.hpp
@@ -6,6 +6,27 @@
 
 namespace mojo {
 
+/**
+
+  Top level elements must have a name attribute
+
+  XML element names are the type names registered in the TypeSystem, properties
+  could use the property name as the element name but that would be inconsitant
+  for collections where each type instance can only be identified by an id where
+  the id is generated from the address of the object.
+
+  Object properties are child elements
+
+  Each object has a unique id number
+
+  A collection contains references to types defined elsewhere 
+
+  Should an Object be defined where it is first referenced, or should references
+  by inserted and the object defined elsewhere.
+
+  Archives are only expected to know about the Object class so all references
+  must be stored as objectref types and then referenced by id 
+ */
 class XMLArchive : public Archive
 {
 public:
