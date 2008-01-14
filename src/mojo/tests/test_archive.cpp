8502f41b5329dc5c3a00f3a9b89e3b641d3704e6
Tim Mayberry <mojofunk@gmail.com>
2008-01-14 10:56:30 +1000

    Rename Archive test and simplify it a little bit

---
+++ src/mojo/tests/test_archive.cpp
@@ -0,0 +1,77 @@
+#define BOOST_TEST_MODULE mojo_archive
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
+//#include <boost/test/test_tools.hpp>
+
+#include <mojo/application.hpp>
+#include <mojo/dummy_archive.hpp>
+
+using namespace boost::unit_test;
+using namespace std;
+using namespace mojo;
+
+template <class T>
+bool
+insert_property(Properties& props, const string& name, const T& value)
+{
+	return props.insert(make_property<string, T>(name, value)).second;
+}
+
+template <class T>
+void
+check_property (const Properties& props, const string& property_name, const T& expected_value)
+{
+	Properties::const_iterator i;
+	BOOST_REQUIRE((i = props.find (property_name)) != props.end());
+
+	T prop_value; 
+
+	BOOST_CHECK_NO_THROW(prop_value = boost::any_cast<T>(i->second));
+
+	BOOST_CHECK_EQUAL(prop_value, expected_value);
+}
+
+void
+insert_some_properties (Properties& props)
+{
+	BOOST_CHECK(insert_property<string>(props, "name", "timbyr"));
+	BOOST_CHECK(insert_property<int32_t>(props, "age", 12));
+	BOOST_CHECK(insert_property<float>(props, "avgloc", 10.53f));
+}
+
+void
+check_some_properties (Properties& props)
+{
+	check_property<std::string>(props, "name", "timbyr");
+	check_property<int32_t>(props, "age", 12);
+	check_property<float>(props, "avgloc", 10.23f);
+}
+
+void
+test_archive (Archive& archive, const string& filename)
+{
+	Properties props;
+
+	insert_some_properties (props);
+
+	BOOST_CHECK_NO_THROW(archive.write (filename, props));
+
+	props.clear();
+
+	BOOST_CHECK_NO_THROW(archive.read (filename, props));
+
+	check_some_properties (props);
+}
+
+BOOST_AUTO_TEST_CASE( dummy_archive_test )
+{
+	int argc = framework::master_test_suite().argc;
+	char** argv = framework::master_test_suite().argv;
+
+	ApplicationSPtr app = Application::create (argc, argv);
+
+	DummyArchive archive;
+
+	test_archive (archive, "dummy.txt");
+}
