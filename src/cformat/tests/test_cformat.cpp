7dc7e334b7ce7aba856365158b792ac5c409e442
Tim Mayberry <mojofunk@gmail.com>
2008-01-14 14:00:30 +1000

    Update and improve cformat test

--- src/cformat/tests/test_cformat.cpp
+++ src/cformat/tests/test_cformat.cpp
@@ -10,11 +10,28 @@ using namespace boost::unit_test;
 using namespace std;
 using namespace cformat;
 
+#define MAX_INT32_T "2147483647"
+#define MIN_INT32_T "-2147483648"
+
 BOOST_AUTO_TEST_CASE( int32_conversion )
 {
-	BOOST_CHECK_EQUAL("233141", convert<string>(233141));
+	BOOST_CHECK_EQUAL(MAX_INT32_T, convert<string>(std::numeric_limits<int32_t>::max()));
+	BOOST_CHECK_EQUAL(std::numeric_limits<int32_t>::max(), convert<int32_t>(string(MAX_INT32_T)));
+	
+	BOOST_CHECK_EQUAL(MIN_INT32_T, convert<string>(std::numeric_limits<int32_t>::min()));
+	BOOST_CHECK_EQUAL(std::numeric_limits<int32_t>::min(), convert<int32_t>(string(MIN_INT32_T)));
+}
 
-	BOOST_CHECK_EQUAL(123456789, convert<int32_t>(string("123456789")));
+#define MAX_INT64_T "9223372036854775807"
+#define MIN_INT64_T "-9223372036854775808"
+
+BOOST_AUTO_TEST_CASE( int64_conversion )
+{
+	BOOST_CHECK_EQUAL(MAX_INT64_T, convert<string>(std::numeric_limits<int64_t>::max()));
+	BOOST_CHECK_EQUAL(std::numeric_limits<int64_t>::max(), convert<int64_t>(string(MAX_INT64_T)));
+	
+	BOOST_CHECK_EQUAL(MIN_INT64_T, convert<string>(std::numeric_limits<int64_t>::min()));
+	BOOST_CHECK_EQUAL(std::numeric_limits<int64_t>::min(), convert<int64_t>(string(MIN_INT64_T)));
 }
 
 BOOST_AUTO_TEST_CASE( float_conversion )
