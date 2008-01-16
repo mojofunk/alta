58a1009de35595b4a8a585d63d97e72c871bd7fd
Tim Mayberry <mojofunk@gmail.com>
2008-01-16 12:34:10 +1000

    Run tests under valgrind and write valgrind output into build directory

--- run_tests.sh
+++ run_tests.sh
@@ -7,5 +7,5 @@
 for file in `find $BUILD_DIR -name 'test_*' -type f -perm /u+x`;
 do
 	echo "Running test....$file"
-	$file "$@";
+	valgrind -v --track-fds=yes --leak-check=full --log-file=$file $file "$@";
 done;
