49a849b5b46f45055abb130ed793c634c6313de1
Tim Mayberry <mojofunk@gmail.com>
2008-01-21 11:43:53 +1000

    Add test for mojo::get_non_existent_file_path

---
+++ src/mojo/tests/test_file_utils.cpp
@@ -0,0 +1,28 @@
+
+#define BOOST_TEST_MODULE mojo_file_utils
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
+
+#include <mojo/file_utils.hpp>
+
+using namespace boost::unit_test;
+using namespace std;
+using namespace mojo;
+
+BOOST_AUTO_TEST_CASE( test_get_non_existant_file_path )
+{
+	const fs::path path("share/projects/motronic/audiofiles/notify.wav"); 
+
+	BOOST_CHECK(fs::exists (path));
+
+	const fs::path new_path(get_non_existent_file_path (path));
+
+	BOOST_CHECK(path != new_path);
+
+	BOOST_CHECK(!fs::exists (new_path));
+
+	const fs::path expected_path("share/projects/motronic/audiofiles/notify-1.wav");
+
+	BOOST_CHECK_EQUAL(new_path, expected_path);
+}
