f0bba5680cf8af8a6cc5804902c33a6c38ca79cc
Tim Mayberry <mojofunk@gmail.com>
2008-04-03 13:13:27 +1000

    Add a Project menu to the edit window, contains only "project-add-audio-track" action

--- src/ui/actions/edit_window_menu_action_entries.hpp
+++ src/ui/actions/edit_window_menu_action_entries.hpp
@@ -12,6 +12,7 @@ const GtkActionEntry edit_window_menu_action_entries[] =
 {
 	{ "file-menu", NULL, N_("_File"), NULL, NULL, NULL },
 	{ "edit-menu", NULL, N_("_Edit"), NULL, NULL, NULL },
+	{ "project-menu", NULL, N_("_Project"), NULL, NULL, NULL },
 	{ "view-menu", NULL, N_("_View"), NULL, NULL, NULL },
 	{ "help-menu", NULL, N_("_Help"), NULL, NULL, NULL },
 
