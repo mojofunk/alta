4766f352765f6be9d6b410e3419edd28cb4fc81e
Tim Mayberry <mojofunk@gmail.com>
2008-04-12 15:23:13 +1000

    Add functions mojo::physical_memory_size and mojo::mlock_limit in new header mojo/resource.hpp

---
+++ src/mojo/resource.cpp
@@ -0,0 +1,48 @@
+
+#ifdef __linux__
+#include <unistd.h>
+#include <sys/resource.h>
+#endif
+
+#include <limits>
+
+#include <mojo/resource.hpp>
+
+namespace mojo {
+
+int64_t
+physical_memory_size ()
+{
+
+#ifdef __linux__
+	long pages, page_size;
+
+	if ((page_size = sysconf (_SC_PAGESIZE)) < 0 || (pages = sysconf (_SC_PHYS_PAGES)) < 0) {
+		return 0;
+	}
+	return (int64_t) pages * (int64_t) page_size;
+#endif
+
+	return 0;
+}
+
+int64_t
+mlock_limit()
+{
+
+#ifdef __linux__
+	struct rlimit limits;
+
+	if (getrlimit (RLIMIT_MEMLOCK, &limits) != 0) return 0;
+
+	if (limits.rlim_cur != RLIM_INFINITY) {
+		return std::numeric_limits<int64_t>::max();
+	}
+
+	return limits.rlim_cur;
+#endif
+
+	return 0;
+}
+
+} // namespace mojo
