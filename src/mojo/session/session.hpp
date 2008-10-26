5987a2465098501be9a2e03669e5799193b7b53e
Tim Mayberry <mojofunk@gmail.com>
2008-10-26 19:06:07 +1000

    Add some documentation to Session class

--- src/mojo/session/session.hpp
+++ src/mojo/session/session.hpp
@@ -27,6 +27,28 @@ namespace mojo {
  *
  * The Session thread also dispatches events to the session event bus. The
  * session event bus is how the clients recieve all asyncronous messages.
+ *
+ * A new project must be able to be created without needing to specify
+ * a project/recording directory. This implies that when enabling record
+ * that it should return an error status that allows the client to set a
+ * record directory and try again.
+ *
+ * Several projects must be able to be open at once but only one project
+ * can be active at once.
+ *
+ * Only an active project can be modified?
+ *
+ * methods like save_project and close_project don't take a project
+ * identifier argument. Does that mean they apply to the current active
+ * project?
+ *
+ * I think it would simplify things greatly if all objects are managed
+ * by the Session or at least creation and destruction of objects is controlled
+ * through the Session.
+ *
+ * This means that clients can treat any objects that are created through the
+ * session as managed but do not participate in that management.
+ *
  */
 class Session
 {
@@ -40,24 +62,37 @@ public: // public API
 
 	/**
 	 * Create a new project.
+	 * Async
 	 */
-	void new_project (const std::string& project_folder);
+	void new_project ();
 
 	/**
 	 * If the native samplerate of the project
 	 * and the rate of the AudioDevice are different
 	 * then the audio device is re-opened to match
 	 * the native rate.
+	 *
+	 * Async
 	 */
 	void open_project (const std::string& project_file);
 
+	/**
+	 * Async
+	 */
 	void save_project_as (const std::string& filename);
 
-	/// should return error status if project file name
-	/// hasn't been set
+	/**
+	 * Will send an error to the session bus if the if
+	 * project file name hasn't been set
+	 *
+	 * Async
+	 */
 	void save_project ();
 
-	/// should return status
+	/**
+	 * should return status
+	 * Async
+	 */
 	void close_project ();
 
 	// Access to auditioning functions
