5b34195df1748ae07b850f960b87c32fe8854bdd
Tim Mayberry <mojofunk@gmail.com>
2009-06-26 13:55:06 +1000

    Remove TrackCanvasFactory class

--- src/mojo-gtkmm/track_view.cpp
+++ src/mojo-gtkmm/track_view.cpp
@@ -3,7 +3,7 @@
 
 #include "app.hpp"
 #include "bus.hpp"
-#include "track_canvas_factory.hpp"
+#include "track_canvas.hpp"
 #include "track_view_item_factory.hpp"
 #include "track_list.hpp"
 #include "track_list_item.hpp"
@@ -15,7 +15,7 @@ namespace ui {
 TrackView::TrackView(mojo::Project* p)
 	: m_project(p)
 	, m_track_list(new TrackList)
-	, m_canvas(TrackCanvasFactory::create (p))
+	, m_canvas(new TrackCanvas())
 {
 	App::get_session_bus().signal_track_added().connect (
 			sigc::mem_fun (this, &TrackView::on_track_added));
