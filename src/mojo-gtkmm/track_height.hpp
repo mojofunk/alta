6a331a67382dc799c0dbc929ee3b676ef7704ff0
Tim Mayberry <mojofunk@gmail.com>
2010-11-21 18:45:22 +1000

    Add Track height related function declarations

---
+++ src/mojo-gtkmm/track_height.hpp
@@ -0,0 +1,24 @@
+#ifndef UI_TRACK_HEIGHT
+#define UI_TRACK_HEIGHT
+
+namespace ui {
+
+enum Height {
+	HeightSmallest,
+	HeightSmall,
+	HeightNormal,
+	HeightLarge,
+	HeightLargest
+};
+
+int get_height_in_pixels (Height);
+
+void set_default_height (Height);
+
+Height get_default_height ();
+
+std::string get_height_name (Height);
+
+}
+
+#endif
