3b8443647fd84fe4194f0695f35acd2bfb387772
Tim Mayberry <mojofunk@gmail.com>
2012-05-25 11:34:03 +1000

    Move notes on Application::import_audiofiles

--- src/mojo/api/application.hpp
+++ src/mojo/api/application.hpp
@@ -188,6 +188,22 @@ public: // public API
 
 	static bool is_audio_track (Track*);
 
+	/**
+	 * task_t is an identifier for a particular task
+	 *
+	 * The task_t can be used to discover the status of task,
+	 * cancel a task etc.
+	 *
+	 * import options:
+	 *
+	 * copy files to audio folder
+	 * split multi-channel files into mono files
+	 *
+	 * The files are added to the project that is active for the application
+	 *
+	 */
+	//task_t import_audiofiles (const paths& files, const ImportOptions&)
+
 public: // transport API
 
 	void transport_set_speed (float speed);
