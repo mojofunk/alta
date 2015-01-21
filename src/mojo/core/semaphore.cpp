dc36bd1ccf35ca7dadcddf65e82aba9c43d44501
Tim Mayberry <mojofunk@gmail.com>
2015-01-21 14:23:22 +0900

    Add mojo::Semaphore class, like gleam::Semaphore but using std:: types

---
+++ src/mojo/core/semaphore.cpp
@@ -0,0 +1,52 @@
+
+#include "mojo/core/semaphore.hpp"
+
+namespace mojo {
+
+Semaphore::Semaphore (uint32_t initial_val)
+	:
+		m_counter(initial_val)
+{ }
+
+void
+Semaphore::wait ()
+{
+	std::unique_lock<std::mutex> lock(m_mutex);
+	while (m_counter < 1) {
+		m_cond.wait(lock);
+	}
+
+	--m_counter;
+}
+
+bool
+Semaphore::try_wait ()
+{
+	std::unique_lock<std::mutex> lock(m_mutex, std::defer_lock);
+	try {
+		if (!lock.try_lock())
+		{
+			return false;
+		}
+	} catch (...) {
+		return false;
+	}
+	// lock successful
+	while (m_counter < 1) {
+		m_cond.wait(lock);
+	}
+
+	// should be able to decrement after unlock
+	// but it doesn't really matter
+	--m_counter;
+	return true;
+}
+
+void
+Semaphore::post ()
+{
+	++m_counter;
+	m_cond.notify_one();
+}
+
+} // namespace mojo
---
+++ src/mojo/core/semaphore.hpp
@@ -0,0 +1,61 @@
+
+#ifndef MOJO_CORE_SEMAPHORE
+#define MOJO_CORE_SEMAPHORE
+
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+namespace mojo {
+
+class Semaphore
+{
+public:
+
+	// prevent copying and assignment
+	Semaphore(const Semaphore& sema) = delete;
+	Semaphore& operator= (const Semaphore& sema) = delete;
+
+public:
+
+	/**
+	 * \param initial_val The initial value of the semaphore,
+	 * defaults to 1(binary semaphore)
+	 */
+	Semaphore (uint32_t initial_val = 1);
+
+	/**
+	 * Aquire a "permit" from the semaphore, if one is
+	 * available immediately return after reducing the
+	 * number of permits by 1. If a permit isn't available
+	 * then the thread will be put into a blocked state
+	 * until another thread calls release.
+	 */
+	void wait ();
+
+	/**
+	 * Try and aquire a permit. This is a non-blocking operation.
+	 *
+	 * \return true if a permit was able to be aquired, false
+	 * if otherwise.
+	 */
+	bool try_wait ();
+
+	/**
+	 * Release a permit increasing the number of permits
+	 * by one. This is a non-blocking operation.
+	 */
+	void post ();
+
+private:
+
+	// counter is atomic so post doesn't need to take lock to post/notify
+	std::atomic<uint32_t>    m_counter;
+	std::condition_variable  m_cond;
+	std::mutex               m_mutex;
+
+};
+
+} // namespace mojo
+
+#endif // MOJO_CORE_SEMAPHORE
