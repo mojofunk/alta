0fe21a605fd1ce5aa3322192836645f57d78096b
Tim Mayberry <mojofunk@gmail.com>
2015-03-02 21:37:59 +1000

    Add macros for debugging timing

--- src/mojo/core/debug.hpp
+++ src/mojo/core/debug.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "mojo/core/debug_registry.hpp"
+#include "mojo/core/timing.hpp"
 
 namespace mojo {
 
@@ -24,10 +25,28 @@ namespace debug {
 
 } // namespace mojo
 
+#ifdef NDEBUG
+
+#define MOJO_DEBUG_DOMAIN(domain_name)
+#define MOJO_DEBUG_MSG(domain, str)
+#define MOJO_DEBUG(domain)
+
+#define MOJO_DEBUG_TIMING_START(domain,td)
+#define MOJO_DEBUG_TIMING_ADD_ELAPSED(domain,td)
+#define MOJO_DEBUG_TIMING_RESET(domain,td)
+
+#else // DEBUG
+
 #define MOJO_DEBUG_DOMAIN(domain_name) namespace { uint32_t domain_name = mojo::debug::get_domain_index( # domain_name ); }
 
 #define MOJO_DEBUG_MSG(domain, str) if (mojo::debug::get_enabled(domain)) { mojo::debug::log_message(__FILE__, __LINE__, domain, str); }
 
 #define MOJO_DEBUG(domain) if (mojo::debug::get_enabled(domain)) { mojo::debug::log_message (__FILE__, __LINE__, domain, ""); }
+
+#define MOJO_DEBUG_TIMING_START(domain,td) if (mojo::debug::get_enabled(domain)) { td.start_timing (); }
+#define MOJO_DEBUG_TIMING_ADD_ELAPSED(domain,td) if (mojo::debug::get_enabled(domain)) { td.add_elapsed (); }
+#define MOJO_DEBUG_TIMING_RESET(domain,td) if (mojo::debug::get_enabled(domain)) { td.reset (); }
+
+#endif
 
 #endif // MOJO_CORE_DEBUG_H
