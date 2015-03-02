5bd88d566a1c2e0fb56087a47fea11e3feb2d25c
Tim Mayberry <mojofunk@gmail.com>
2015-03-02 21:41:26 +1000

    Add GUI Timers with timing debug output

--- src/gtkmm_ui/timers.cpp
+++ src/gtkmm_ui/timers.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2007 Tim Mayberry 
+    Copyright (C) 2014 Tim Mayberry
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -17,83 +17,181 @@
 
 */
 
-#include <gleam/timer.hpp>
-
 #include "timers.hpp"
 
-using namespace gleam;
+#include "gleam/timer.hpp"
+
+#include "mojo/core/debug.hpp"
+#include "mojo/string/compose.hpp"
+
+MOJO_DEBUG_DOMAIN(GUI_TIMING);
 
 namespace {
 
-class Timers
+class StandardTimer : public gleam::StandardTimer
+{
+public:
+	StandardTimer (unsigned int interval)
+		: gleam::StandardTimer(interval)
+	{ }
+
+	virtual bool on_elapsed () {
+		MOJO_DEBUG_TIMING_ADD_ELAPSED(GUI_TIMING, timing_interval_data);
+		MOJO_DEBUG_TIMING_START(GUI_TIMING, timing_exec_data);
+
+		bool ret_val = gleam::StandardTimer::on_elapsed ();
+
+		MOJO_DEBUG_TIMING_ADD_ELAPSED(GUI_TIMING, timing_exec_data);
+		MOJO_DEBUG_TIMING_START(GUI_TIMING, timing_interval_data);
+		return ret_val;
+	}
+
+#ifndef NDEBUG
+	mojo::TimingData timing_interval_data;
+	mojo::TimingData timing_exec_data;
+#endif
+};
+
+class BlinkTimer : public gleam::BlinkTimer
+{
+public:
+	BlinkTimer (unsigned int interval)
+		: gleam::BlinkTimer(interval)
+	{ }
+
+	virtual bool on_elapsed () {
+		MOJO_DEBUG_TIMING_ADD_ELAPSED(GUI_TIMING, timing_interval_data);
+		MOJO_DEBUG_TIMING_START(GUI_TIMING, timing_exec_data);
+
+		bool ret_val = gleam::BlinkTimer::on_elapsed ();
+
+		MOJO_DEBUG_TIMING_ADD_ELAPSED(GUI_TIMING, timing_exec_data);
+		MOJO_DEBUG_TIMING_START(GUI_TIMING, timing_interval_data);
+		return ret_val;
+	}
+
+#ifndef NDEBUG
+	mojo::TimingData timing_interval_data;
+	mojo::TimingData timing_exec_data;
+#endif
+};
+
+
+class UITimers
 {
 public:
 
-	Timers();
+	UITimers ()
+		: blink(240)
+		, slow(1000)
+		, fast(100)
+		, rapid(40)
+	{
+#ifndef NDEBUG
+		slow.connect (sigc::mem_fun (*this, &UITimers::on_slow_timer));
+#endif
+	}
 
-	/// signal every second
-	StandardTimer& slow() { return m_slow; }
-	
-	/// signal every tenth of a second
-	StandardTimer& fast() { return m_fast; }
-	
-	/// signal every 40 msec(25 times a second)
-	StandardTimer& rapid() { return m_rapid; }
+	BlinkTimer      blink;
+	StandardTimer   slow;
+	StandardTimer   fast;
+	StandardTimer   rapid;
 
-	BlinkTimer& blink() { return m_blink; }
+#ifndef NDEBUG
+	std::vector<uint64_t> fast_eps_count;
+	std::vector<uint64_t> rapid_eps_count;
 
 private:
 
-	StandardTimer m_slow;
-	StandardTimer m_fast;
-	StandardTimer m_rapid;
+	void debug_fast_timer ()
+	{
+		MOJO_DEBUG_MSG(GUI_TIMING,
+			mojo::compose ("Fast Connections: %1\n",
+				fast.connection_count ()));
 
-	BlinkTimer m_blink;
+		fast_eps_count.push_back (fast.timing_exec_data.size());
+
+		MOJO_DEBUG_MSG(GUI_TIMING,
+			mojo::compose ("Fast Exec Totals: %1",
+				mojo::timing_summary (fast_eps_count)));
+
+		MOJO_DEBUG_MSG(GUI_TIMING,
+			mojo::compose ("Fast Interval: %1",
+				fast.timing_interval_data.summary()));
+
+		MOJO_DEBUG_MSG(GUI_TIMING,
+			mojo::compose ("Fast Exec: %1",
+				fast.timing_exec_data.summary()));
+
+		MOJO_DEBUG_TIMING_RESET(GUI_TIMING, fast.timing_interval_data);
+		MOJO_DEBUG_TIMING_RESET(GUI_TIMING, fast.timing_exec_data);
+	}
+
+	void debug_rapid_timer ()
+	{
+		MOJO_DEBUG_MSG(GUI_TIMING,
+			mojo::compose ("Rapid Connections: %1\n",
+				rapid.connection_count ()));
+
+		rapid_eps_count.push_back (rapid.timing_exec_data.size());
+
+		MOJO_DEBUG_MSG(GUI_TIMING,
+			mojo::compose ("Rapid Exec Totals: %1",
+				mojo::timing_summary (rapid_eps_count)));
+
+		MOJO_DEBUG_MSG(GUI_TIMING,
+			mojo::compose ("Rapid Interval: %1",
+				rapid.timing_interval_data.summary()));
+
+		MOJO_DEBUG_MSG(GUI_TIMING,
+			mojo::compose ("Rapid Exec: %1",
+				rapid.timing_exec_data.summary()));
+
+		MOJO_DEBUG_TIMING_RESET(GUI_TIMING, rapid.timing_interval_data);
+		MOJO_DEBUG_TIMING_RESET(GUI_TIMING, rapid.timing_exec_data);
+
+	}
+
+	void on_slow_timer () {
+		debug_fast_timer ();
+		debug_rapid_timer ();
+	}
+#endif
 };
 
-Timers* timers = 0;
-
-Timers::Timers()
-:
-	m_slow(1000),
-	m_fast(100),
-	m_rapid(40),
-	m_blink(240)
-{ }
-
-Timers&
+UITimers&
 get_timers ()
 {
-	if (!timers)
-	{
-		timers = new Timers;
-	}
-	return *timers;
+	static UITimers timers;
+	return timers;
 }
 
-} // anon
+} // anon namespace
+
+namespace ui {
 
 sigc::connection
 slow_timer_connect (const sigc::slot<void>& slot)
 {
-	return get_timers().slow().connect (slot);
+	return get_timers().slow.connect (slot);
 }
 
 sigc::connection
 fast_timer_connect (const sigc::slot<void>& slot)
 {
-	return get_timers().fast().connect (slot);
+	return get_timers().fast.connect (slot);
 }
 
 sigc::connection
-rapid_timer_connect (const sigc::slot<void>& slot)
+rapid_connect (const sigc::slot<void>& slot)
 {
-	return get_timers().rapid().connect (slot);
+	return get_timers().rapid.connect (slot);
 }
 
 sigc::connection
-blink_timer_connect (const sigc::slot<void, bool>& slot)
-{ 
-	return get_timers().blink().connect (slot);
+blink_connect (const sigc::slot<void,bool>& slot)
+{
+	return get_timers().blink.connect (slot);
 }
 
+} // namespace ui
