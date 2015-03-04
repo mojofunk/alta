fd7b14f50c1b074e8328085c474386f810f0ec9e
Tim Mayberry <mojofunk@gmail.com>
2015-03-04 23:47:33 +1000

    Add script to configure and build all configurations

---
+++ tools/linux/build-all-configs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+./clean.sh
+./configure-debug.sh && ./waf.sh -j2 || exit 1
+./clean.sh
+./configure-debug-amalgamated.sh && ./waf.sh -j2 || exit 1
+./clean.sh
+./configure-debug-static.sh && ./waf.sh -j2 || exit 1
+./clean.sh
+./configure-debug-gui.sh && ./waf.sh -j2 || exit 1
+./clean.sh
+./configure-debug-gui-static.sh && ./waf.sh -j2 || exit 1
+./clean.sh
+./configure-release.sh && ./waf.sh -j2 || exit 1
+./clean.sh
