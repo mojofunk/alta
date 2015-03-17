211779c3bb5fbaa66b3b00259c5fe53a961d3ec4
Tim Mayberry <mojofunk@gmail.com>
2015-03-17 11:57:52 +1000

    Add reminder for when python pep8 formatting tools aren't installed

--- tools/check-wscript-pep8.sh
+++ tools/check-wscript-pep8.sh
@@ -4,4 +4,5 @@ if hash pep8 2>/dev/null; then
     find ../ -type f -name "wscript" | xargs pep8
 else
     echo "pep8 is not installed, cannot check wscript files for pep8 conformity"
+	echo "try $ sudo pip install pep8"
 fi
--- tools/fix-wscript-pep8.sh
+++ tools/fix-wscript-pep8.sh
@@ -4,4 +4,5 @@ if hash pep8 2>/dev/null; then
     find ../ -type f -name "wscript" | xargs autopep8 -i -a
 else
     echo "autopep8 is not installed, cannot fix wscript files for pep8 conformity"
+	echo "try $ sudo pip install autopep8"
 fi
