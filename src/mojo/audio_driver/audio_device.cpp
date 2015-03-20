a690695f2a73809dcea1e4ff7bd4210ad5b630c6
Tim Mayberry <mojofunk@gmail.com>
2015-03-21 09:39:11 +1000

    Add definition of AudioDevice destructor
    This is required with windows mingw build

---
+++ src/mojo/audio_driver/audio_device.cpp
@@ -0,0 +1,10 @@
+#ifndef MOJO_CORE_AMALGAMATED
+#include "mojo/core/config/common_source_includes.hpp"
+#include "mojo/audio_driver/audio_device.hpp"
+#endif
+
+namespace mojo {
+
+AudioDevice::~AudioDevice () { }
+
+} // namespace mojo
