ab780643efc5f52d4f76024839cebbadb29dfd67
Tim Mayberry <mojofunk@gmail.com>
2015-03-23 00:17:28 +1000

    Improve header include paths
    application.hpp needs the types defined in audio_driver/typedefs.hpp
    If audio_driver/typedefs.hpp includes mojo-audio-driver.hpp then
    when the other headers are being included the types in typedefs.hpp
    aren't defined because the header include guards prevent typedefs.hpp
    being included again. So no cycles in header includes.

--- src/mojo/audio_driver/typedefs.hpp
+++ src/mojo/audio_driver/typedefs.hpp
@@ -1,11 +1,9 @@
 #ifndef MOJO_AUDIO_DRIVER_TYPEDEFS_H
 #define MOJO_AUDIO_DRIVER_TYPEDEFS_H
 
-#ifndef MOJO_CORE_AMALGAMATED
-#include "mojo/core/config/common_header_includes.hpp"
-#include "mojo/core/typesystem/smart_pointer_macros.hpp"
-#include "mojo/audio_driver/forward.hpp"
-#endif
+#include <mojo/core/config/common_header_includes.hpp>
+#include <mojo/core/typesystem/smart_pointer_macros.hpp>
+#include <mojo/audio_driver/forward.hpp>
 
 namespace mojo {
 
