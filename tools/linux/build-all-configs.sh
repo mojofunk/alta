d2591c62e35a8742409235c6b74a588d56cd796e
Tim Mayberry <mojofunk@gmail.com>
2015-03-23 13:18:17 +1000

    Disable building gtkmm_ui until waf/python issue on Fedora 21 is fixed

--- tools/linux/build-all-configs.sh
+++ tools/linux/build-all-configs.sh
@@ -3,19 +3,19 @@
 ./clean.sh
 ./configure-debug.sh && ./waf.sh -j2 || exit 1
 ./clean.sh
-./configure-debug-static.sh && ./waf.sh -j2 || exit 1
-./clean.sh
 ./configure-debug-amalgamated.sh && ./waf.sh -j2 || exit 1
 ./clean.sh
 ./configure-debug-tests.sh && ./waf.sh -j2 || exit 1
 ./clean.sh
+./configure-debug-tests-static.sh && ./waf.sh -j2 || exit 1
+./clean.sh
 ./configure-debug-tests-amalgamated.sh && ./waf.sh -j2 || exit 1
 ./clean.sh
-./configure-debug-gtkmm-ui.sh && ./waf.sh -j2 || exit 1
-./clean.sh
-./configure-debug-gtkmm-ui-static.sh && ./waf.sh -j2 || exit 1
-./clean.sh
-./configure-debug-gtkmm-ui-amalgamated.sh && ./waf.sh -j2 || exit 1
-./clean.sh
+#./configure-debug-gtkmm-ui.sh && ./waf.sh -j2 || exit 1
+#./clean.sh
+#./configure-debug-gtkmm-ui-static.sh && ./waf.sh -j2 || exit 1
+#./clean.sh
+#./configure-debug-gtkmm-ui-amalgamated.sh && ./waf.sh -j2 || exit 1
+#./clean.sh
 ./configure-release.sh && ./waf.sh -j2 || exit 1
 ./clean.sh
