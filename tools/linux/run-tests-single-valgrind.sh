154ec22adabc48b38aa7b98a2e3b3e0e2a6dde0e
Tim Mayberry <mojofunk@gmail.com>
2015-08-03 09:56:16 +1000

    Rename scripts to run tests and add variant that doesn't run under valgrind
    Running under valgrind is much much slower and can be tedious when you just
    want the results of the tests and not valgrind related output

--- tools/linux/run-tests-single-valgrind.sh
+++ tools/linux/run-tests-single-valgrind.sh
@@ -15,5 +15,6 @@ fi
 for file in `find $BUILD_DIR -name "$TESTS" -type f -perm /u+x`;
 do
 	echo "Running test....$file"
-	$file --log_level="$LOG_LEVEL" "$@";
+	G_DEBUG=gc-friendly G_SLICE=always-malloc valgrind -v --track-fds=yes \
+	--leak-check=full --log-file=$file.%p $file --log_level="$LOG_LEVEL" "$@";
 done;
--- tools/linux/run-tests-valgrind.sh
+++ tools/linux/run-tests-valgrind.sh
@@ -4,10 +4,11 @@
 
 . ./env.sh
 
-LOG_LEVEL='all' # error is the default
+LOG_LEVEL='test_suite' # error is the default
 
 for file in `find $BUILD_DIR -name "mojo-tests" -type f -perm /u+x`;
 do
 	echo "Running test....$file"
-	$file --log_level="$LOG_LEVEL" "$@";
+	G_DEBUG=gc-friendly G_SLICE=always-malloc valgrind -v --track-fds=yes \
+	--leak-check=full --log-file=$file.%p $file --log_level="$LOG_LEVEL" "$@";
 done;
