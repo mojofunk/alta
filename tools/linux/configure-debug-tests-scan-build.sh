3c07eea7e4d9795db5639fcfa8d85fd7a07ca10e
Tim Mayberry <mojofunk@gmail.com>
2015-09-06 11:19:09 +1000

    Remove some unused scripts now that similar functionality is in alta-build

--- tools/linux/configure-debug-tests-scan-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. ./env.sh
-
-cd $BASE || exit 1
-scan-build ./waf configure --with-tests "$@"
--- tools/linux/debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-. ./env.sh
-
-cd $BASE || exit 1
-exec gdb $EXECUTABLE "$@"
