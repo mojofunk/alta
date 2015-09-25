20579816809d8f68c5de6c68371f31f794d513fd
Tim Mayberry <mojofunk@gmail.com>
2015-09-25 12:54:23 +1000

    Perform boost lockfree queue test for 10 iterations
    This fails quite frequently, not sure what is going on at this stage, push
    count can be greater than, equal to or less that pop count which indicates that
    failure in atomics somehow? or programming error, but my code is basically
    identical to the example code.

--- src/mojo/tests/test_boost_lockfree.cpp
+++ src/mojo/tests/test_boost_lockfree.cpp
@@ -27,10 +27,10 @@ BOOST_AUTO_TEST_CASE(test_spsc_queue)
 const int queue_size = 100;
 queue<int, capacity<queue_size>> q;
 
-std::atomic_int push_count(0);
-std::atomic_int pop_count(0);
+static std::atomic<int> push_count(0);
+static std::atomic<int> pop_count(0);
 
-std::atomic_bool done(false);
+static std::atomic<bool> done(false);
 
 void produce()
 {
@@ -56,14 +56,19 @@ void consume()
 	}
 }
 
-BOOST_AUTO_TEST_CASE(test_queue)
+void
+check_push_pop_count ()
 {
-	BOOST_CHECK(q.is_lock_free());
+	done = false;
+	push_count = 0;
+	pop_count = 0;
 
 	std::thread t1(produce);
 	std::thread t2(produce);
-	std::thread t3(consume);
+	std::thread t3(produce);
 	std::thread t4(consume);
+	std::thread t5(consume);
+	std::thread t6(consume);
 
 	mojo::sleep(5);
 
@@ -72,10 +77,22 @@ BOOST_AUTO_TEST_CASE(test_queue)
 	t2.join();
 	t3.join();
 	t4.join();
+	t5.join();
+	t6.join();
 
 	std::cerr << "push_count = " << push_count << std::endl;
 	std::cerr << "pop_count = " << pop_count << std::endl;
 	BOOST_CHECK(push_count == pop_count);
+	BOOST_CHECK(q.empty());
+}
+
+BOOST_AUTO_TEST_CASE(test_queue)
+{
+	BOOST_CHECK(q.is_lock_free());
+
+	for (int i = 0; i < 10; ++i) {
+		check_push_pop_count();
+	}
 }
 
 BOOST_AUTO_TEST_CASE(test_stack)
