c7d87c1b1b051afb6545257ab665791f5561809d
Tim Mayberry <mojofunk@gmail.com>
2015-10-14 12:17:26 +1000

    Add a set of macros for defining and accessing private implementation
    Add a basic test

---
+++ src/mojo/core/object/private_macros.hpp
@@ -0,0 +1,24 @@
+#ifndef MOJO_CORE_PRIVATE_MACROS_H
+#define MOJO_CORE_PRIVATE_MACROS_H
+
+template <typename T> static inline T* mojo_get_ptr_helper(T* ptr) { return ptr; }
+
+#define M_DECLARE_PRIVATE(Class) \
+	inline Class##Private* d_func() { return reinterpret_cast<Class##Private*>(mojo_get_ptr_helper(d_ptr.get())); } \
+	inline const Class##Private* d_func() const { return reinterpret_cast<const Class##Private*>(mojo_get_ptr_helper(d_ptr.get())); } \
+	friend class Class##Private;
+
+#define M_DECLARE_PRIVATE_D(Dptr, Class) \
+	inline Class##Private* d_func() { return reinterpret_cast<Class##Private *>(Dptr); } \
+	inline const Class##Private* d_func() const { return reinterpret_cast<const Class##Private*>(Dptr); } \
+	friend class Class##Private;
+
+#define M_DECLARE_PUBLIC(Class)                                    \
+	inline Class* q_func() { return static_cast<Class *>(q_ptr); } \
+	inline const Class* q_func() const { return static_cast<const Class *>(q_ptr); } \
+	friend class Class;
+
+#define M_D(Class) Class##Private* const d = d_func()
+#define M_Q(Class) Class* const q = q_func()
+
+#endif // MOJO_CORE_PRIVATE_MACROS_H
---
+++ src/mojo/tests/test_private_macros.cpp
@@ -0,0 +1,178 @@
+#ifndef MOJO_SINGLE_TEST_EXE
+#define BOOST_TEST_MODULE mojo_private_macros
+#endif
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
+
+#include <iostream>
+#include <string>
+
+#include "mojo/core/object/private_macros.hpp"
+
+class ApplicationPrivate;
+
+class Application {
+public:
+	static std::string directory_path();
+	static std::string file_path();
+
+	static std::string name();
+	static std::string executable_name();
+
+	static void initialize ();
+	static void deinitialize ();
+
+private:
+	Application();
+
+private:
+	static Application* instance;
+
+	std::unique_ptr<ApplicationPrivate> d_ptr;
+
+	M_DECLARE_PRIVATE(Application)
+};
+
+class ApplicationPrivate {
+public:
+	ApplicationPrivate()
+	    : m_directory_path("private_test_directory_path")
+	    , m_name("private_test_name")
+	    , m_executable_name("private_test_executable_name")
+	{
+	}
+
+	std::string m_directory_path;
+	std::string m_name;
+	std::string m_executable_name;
+
+	std::string file_path()
+	{
+		// would need to be a path separator etc
+		return m_directory_path + m_executable_name;
+	}
+};
+
+Application* Application::instance;
+
+Application::Application()
+    : d_ptr(new ApplicationPrivate())
+{
+}
+
+void Application::initialize()
+{
+	assert (!instance);
+	instance = new Application;
+}
+
+void Application::deinitialize()
+{
+	delete instance;
+}
+
+std::string Application::directory_path()
+{
+	ApplicationPrivate* d = instance->d_ptr.get();
+	return d->m_directory_path;
+}
+
+std::string Application::file_path()
+{
+	ApplicationPrivate* d = instance->d_ptr.get();
+	return d->file_path();
+}
+
+std::string Application::name()
+{
+	ApplicationPrivate* d = instance->d_ptr.get();
+	return d->m_name;
+}
+
+std::string Application::executable_name()
+{
+	ApplicationPrivate* d = instance->d_ptr.get();
+	return d->m_executable_name;
+}
+
+
+BOOST_AUTO_TEST_CASE(private_no_inheritance_test)
+{
+	Application::initialize();
+
+	std::cout << Application::name() << std::endl;
+	std::cout << Application::directory_path() << std::endl;
+	std::cout << Application::file_path() << std::endl;
+	std::cout << Application::executable_name() << std::endl;
+
+	Application::deinitialize();
+}
+
+class BasePrivate;
+
+class Base {
+protected:
+	Base (BasePrivate*);
+
+	virtual ~Base () { }
+
+protected:
+	std::unique_ptr<BasePrivate> d_ptr;
+
+	M_DECLARE_PRIVATE(Base)
+};
+
+class BasePrivate {
+public:
+	virtual ~BasePrivate() = 0;
+public:
+	Base* q_ptr;
+private:
+	M_DECLARE_PUBLIC(Base)
+public:
+};
+
+BasePrivate::~BasePrivate()
+{
+}
+
+Base::Base (BasePrivate* priv_ptr)
+	: d_ptr(priv_ptr)
+{
+	d_ptr->q_ptr = this;
+}
+
+class DerivedPrivate;
+
+class Derived : public Base {
+	M_DECLARE_PRIVATE(Derived)
+protected:
+	Derived(DerivedPrivate*);
+
+public:
+	Derived ();
+};
+
+class DerivedPrivate : public BasePrivate {
+	M_DECLARE_PUBLIC(Derived)
+public:
+
+};
+
+Derived::Derived()
+    : Base(new DerivedPrivate)
+{
+}
+
+Derived::Derived(DerivedPrivate* priv)
+	: Base(priv)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE(private_inheritance_test)
+{
+	Derived derived;
+
+}
