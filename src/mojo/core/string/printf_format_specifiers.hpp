3242e0183d0e9b1dac6f6b0cea0803e9e6b5905d
Tim Mayberry <mojofunk@gmail.com>
2015-11-02 12:07:10 +1000

    Add printf and scanf format specifiers from cformat library

---
+++ src/mojo/core/string/printf_format_specifiers.hpp
@@ -0,0 +1,75 @@
+#ifndef MOJO_PRINTF_FORMAT_SPECIFIERS_H
+#define MOJO_PRINTF_FORMAT_SPECIFIERS_H
+
+#include <cinttypes>
+
+namespace mojo {
+
+template <typename T>
+const char* const
+printf_format_specifier (const T&)
+{
+	return T::PRINTF_FORMAT_SPECIFIER_UNDEFINED
+}
+
+template <>
+const char* const
+printf_format_specifier (const int16_t&)
+{
+	return "%" PRIi16;
+}
+
+template <>
+const char* const
+printf_format_specifier (const int32_t&)
+{
+	return "%" PRIi32;
+}
+
+template <>
+const char* const
+printf_format_specifier (const int64_t&)
+{
+	return "%" PRIi64;
+}
+
+template <>
+const char* const
+printf_format_specifier (const uint16_t&)
+{
+	return "%" PRIu16;
+}
+
+template <>
+const char* const
+printf_format_specifier (const uint32_t&)
+{
+	return "%" PRIu32;
+}
+
+template <>
+const char* const
+printf_format_specifier (const uint64_t&)
+{
+	return "%" PRIu64;
+}
+
+template <>
+const char* const
+printf_format_specifier (const float&)
+{
+	// I think this is the default anyway?
+	return "%.6g";
+}
+
+template <>
+const char* const
+printf_format_specifier (const double&)
+{
+	// make sure full precision is retained.
+	return "%.12g";
+}
+
+} // namespace mojo
+
+#endif // MOJO_PRINTF_FORMAT_SPECIFIERS_H
---
+++ src/mojo/core/string/scanf_format_specifiers.hpp
@@ -0,0 +1,73 @@
+#ifndef MOJO_SCANF_FORMAT_SPECIFIERS_H
+#define MOJO_SCANF_FORMAT_SPECIFIERS_H
+
+#include <cinttypes>
+
+namespace mojo {
+
+template <typename T>
+const char* const
+scanf_format_specifier (const T&)
+{
+	return 0;
+}
+
+template <>
+const char* const
+scanf_format_specifier (const int16_t&)
+{
+	return "%" SCNi16;
+}
+
+template <>
+const char* const
+scanf_format_specifier (const int32_t&)
+{
+	return "%" SCNi32;
+}
+
+template <>
+const char* const
+scanf_format_specifier (const int64_t&)
+{
+	return "%" SCNi64;
+}
+
+template <>
+const char* const
+scanf_format_specifier (const uint16_t&)
+{
+	return "%" SCNu16;
+}
+
+template <>
+const char* const
+scanf_format_specifier (const uint32_t&)
+{
+	return "%" SCNu32;
+}
+
+template <>
+const char* const
+scanf_format_specifier (const uint64_t&)
+{
+	return "%" SCNu64;
+}
+
+template <>
+const char* const
+scanf_format_specifier (const float&)
+{
+	return "%f";
+}
+
+template <>
+const char* const
+scanf_format_specifier (const double&)
+{
+	return "%f";
+}
+
+} // namespace mojo
+
+#endif // MOJO_SCANF_FORMAT_SPECIFIERS_H
