fb0f37d6e7e145de45df060f1074f7f4c86d629b
Tim Mayberry <mojofunk@gmail.com>
2015-12-02 20:31:10 +1000

    Remove unused code from gleam library

--- src/gleam/gleam.hpp
+++ src/gleam/gleam.hpp
@@ -5,9 +5,7 @@
 
 #include "gleam/atomic_counter.hpp"
 #include "gleam/atomic_ptr.hpp"
-#include "gleam/dispatcher.hpp"
 #include "gleam/semaphore.hpp"
-#include "gleam/terminate_process.hpp"
 #include "gleam/timer.hpp"
 
 #include "gleam/async_signal.hpp"
