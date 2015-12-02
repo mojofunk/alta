0dc8215accefa4febab9d49cb0a410dcf2752183
Tim Mayberry <mojofunk@gmail.com>
2015-12-02 18:35:39 +1000

    Add ability to run alta gtkmm ui under gdb and valgrind using tools/run.sh

--- tools/linux/run.sh
+++ tools/linux/run.sh
@@ -2,5 +2,23 @@
 
 . ./env.sh
 
+OPTION=""
+if [ "$1" == "--debug" -o "$1" == "--valgrind" -o "$1" == "--massif" ]; then
+	OPTION=$1
+	echo "Using option $OPTION"
+	shift 1
+fi
+
 cd $BASE || exit 1
-exec $EXECUTABLE "$@"
+
+if [ "$OPTION" == "--debug" ]; then
+	gdb --args $EXECUTABLE
+elif [ "$OPTION" == "--valgrind" ]; then
+	MEMCHECK_OPTIONS="--leak-check=full --show-leak-kinds=all"
+	valgrind $MEMCHECK_OPTIONS $EXECUTABLE
+elif [ "$OPTION" == "--massif" ]; then
+	MASSIF_OPTIONS="--time-unit=ms --massif-out-file=massif.out.$EXECUTABLE"
+	valgrind --tool=massif $MASSIF_OPTIONS $EXECUTABLE
+else
+	exec $EXECUTABLE "$@"
+fi
