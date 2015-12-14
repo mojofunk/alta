f643e89b7d7b54264af7b8e1887cdf353ce6bdac
Tim Mayberry <mojofunk@gmail.com>
2015-12-14 11:02:53 +1000

    Shorten MOJO_STRFUNC macro to just M_STRFUNC

--- src/mojo/core/macros/strfunc.hpp
+++ src/mojo/core/macros/strfunc.hpp
@@ -2,11 +2,11 @@
 #define MOJO_STRFUNC_H
 
 #if defined(__GNUC__)
-#define MOJO_STRFUNC ((const char*)(__PRETTY_FUNCTION__))
+#define M_STRFUNC ((const char*)(__PRETTY_FUNCTION__))
 #elif defined(_MSC_VER)
-#define MOJO_STRFUNC ((const char*)(__FUNCSIG__))
+#define M_STRFUNC ((const char*)(__FUNCSIG__))
 #else
-#define MOJO_STRFUNC ((const char*)(__func__))
+#define M_STRFUNC ((const char*)(__func__))
 #endif
 
 #endif // MOJO_STRFUNC_H
